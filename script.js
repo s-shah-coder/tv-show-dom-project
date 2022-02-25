151e46566287dbe52c5712f36132f4cf2b9bb7cc
s-shah-coder <saf.h.shah@gmail.com>
2022-02-25 11:21:00 +0000

    level 100 complete

--- script.js
+++ script.js
@@ -9,22 +9,33 @@ function makePageForEpisodes(episodeList) {
   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   const eachEpisode = document.createElement("div");
 
-  // eachEpisode.className = "eachBox"
+  eachEpisode.className = "eachBox"
 
   episodeList.map((element) => {
     
-  const show = document.createElement("div")
-  const h3 = document.createElement("h3");
-  const img = document.createElement("img");
-  const episodeInfo = document.createElement("p")
+    const show = document.createElement("div")
+    const h3 = document.createElement("h3");
+    const img = document.createElement("img");
+    const episodeInfo = document.createElement("p")
 
-  eachEpisode.className = "eachBox"
+    // eachEpisode.className = "eachBox"
 
-  
-  
+    rootElem.appendChild(eachEpisode);
+    eachEpisode.appendChild(show);
+    show.appendChild(h3);
+    show.appendChild(img);
+    show.appendChild(episodeInfo);
 
-  })
+    show.className = "episodeShow";
+    h3.className = "episodeTitle";
+    episodeInfo.className = "summary";
+    img.className = "img";
 
+    h3.innerHTML = ` ${element.name} - S0${element.season}E0${element.number} `;
+    img.src = `${element.image.medium} `;
+    episodeInfo.innerHTML = `${element.summary}`;
+    return element;
+  });
 }
 
 window.onload = setup;
