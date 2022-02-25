dbe0f3b585870968314696e0124120c1e835d27a
s-shah-coder <saf.h.shah@gmail.com>
2022-02-25 19:53:39 +0000

    Revert "added grid to style.css"
    This reverts commit 6e180ddff2fe6609fdce27f0e39dca7feb785ec2.

--- script.js
+++ script.js
@@ -6,25 +6,7 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  const eachEpisode = document.createElement("div");
-
-  // eachEpisode.className = "eachBox"
-
-  episodeList.map((element) => {
-    
-  const show = document.createElement("div")
-  const h3 = document.createElement("h3");
-  const img = document.createElement("img");
-  const episodeInfo = document.createElement("p")
-
-  eachEpisode.className = "eachBox"
-
-  
-  
-
-  })
-
+  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
