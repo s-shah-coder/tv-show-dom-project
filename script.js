5c1bab1a146bca8adb0f84d9e8804d9659217169
s-shah-coder <saf.h.shah@gmail.com>
2022-08-17 17:38:46 +0100

    changed footer data

--- script.js
+++ script.js
@@ -1,63 +1,9 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
-
-const filteredShows = getAllEpisodes();
-const rootElem = document.getElementById("root");
-const header = document.createElement("div");
-let search = document.createElement("input");
-const grid = document.createElement("div");
-grid.className = "eachBox";
-header.appendChild(search);
-rootElem.appendChild(header);
-
-header.className = "header";
-search.className = "search";
-
-function output() {
-  search.addEventListener("keyup", (e) => {
-    const res = e.target.value;
-    const filtered = filteredShows.filter((movie) => {
-      return movie.name.includes(res) || movie.summary.includes(res);
-    });
-
-    return filtered;
-  });
-}
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  const eachEpisode = document.createElement("div");
-
-  eachEpisode.className = "eachBox";
-
-  episodeList.map((element) => {
-    const show = document.createElement("div");
-    const h3 = document.createElement("h3");
-    const img = document.createElement("img");
-    const episodeInfo = document.createElement("p");
-
-    // eachEpisode.className = "eachBox"
-
-    rootElem.appendChild(eachEpisode);
-    eachEpisode.appendChild(show);
-    show.appendChild(h3);
-    show.appendChild(img);
-    show.appendChild(episodeInfo);
-
-    show.className = "episodeShow";
-    h3.className = "episodeTitle";
-    episodeInfo.className = "summary";
-    img.className = "img";
-
-    h3.innerHTML = ` ${element.name} - S0${element.season}E0${element.number} `;
-    img.src = `${element.image.medium} `;
-    episodeInfo.innerHTML = `${element.summary}`;
-    return element;
-  });
-}
-
-window.onload = setup;
+// Variables
+const cardContainerElem = document.querySelector(".card-container");
+const showSearch = document.querySelector("#showSearch");
+const selectSearch = document.getElementById("selectSearch");
+const liveSearch = document.getElementById("liveSearch");
+const episodeDisplayCount = document.getElementById("episodeDisplayCount");
+const filteringDiv = document.getElementById("filtering-text");
+const showLiveSearch = document.getElementById("showLiveSearch");
+const showDisplayCount = document.getElementById("showDisplayCount");
